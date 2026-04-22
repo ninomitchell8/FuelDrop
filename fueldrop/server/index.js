c2434bc6376889f3bf3fe9e6e4d8d3d86eda6038
ninomitchell8 <105947856+ninomitchell8@users.noreply.github.com>
2026-04-22 20:59:10 +0000

    Duplicate app.listen removed. Cors updated for render

--- fueldrop/server/index.js
+++ fueldrop/server/index.js
@@ -9,17 +9,18 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT|| 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
 app.set("trust proxy", 1);
 
-app.use(cors({
+const corsOptions = {
   origin: "https://fuel-drop-nu.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
-}));
+};
 
-app.options("*", cors());
+app.use(cors(corsOptions));
+
+app.options(/.*/, cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
@@ -436,5 +437,5 @@ app.post("/store-user-data", auth, (req, res) => {
 
 
 app.listen (PORT,() => {
-    console.log("yes yes");
+    console.log(`Server running on ${PORT}`);
 });
